28d81d233f080b57b34f443508ba27fbb8c8969b
Corrina-Busby <corrinabuzzy@gmail.com>
2019-08-11 16:19:38 +1200

    function practice

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+// The following are in class JS teachings
+// const - constance something where the value will not change
+
 let name = 'Corrina'
 console.log(name)
 
@@ -6,6 +10,8 @@ const numTwo = 5
 
 const result = numTwo % numOne
 console.log(result)
+
+// switch statement
 
 let groceryItem = 'papaya';
 
@@ -26,3 +32,45 @@ switch(groceryItem) {
 let isNightTime = false;
 
 isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the light!');
+
+//DEFAULT PARAMETERS allow parameters to have a predetermined value in case there is 
+//no argument passed into the function or if the argument is undefined when called.
+
+function greeting (name = 'stranger'){
+    console.log(`Hello, ${name}!`);
+}
+
+greeting('Nick');  //output; Hello, Nick!
+greeting(); //output: Hello, stranger!
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+
+  makeShoppingList();
+
+//In the terminal input 'node index.js' to run in terminal <don't forget to save>
+// RETURN
+function rectangleArea(width, height) {
+    if (width < 1 || height < 1) {
+      return 'You need positive integers to calculate area!';
+    }
+    return width * height;
+  }
+  console.log(rectangleArea(0, 0));
+
+  // The return keyword is powerful because it allows functions to produce an output.
+  // We can then save the output to a variable for later use. 
+
+  function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  const numOfMonitors = monitorCount(5, 4);
+  
+  console.log(numOfMonitors);
+
+
+
